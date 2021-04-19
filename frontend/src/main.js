c7ec4b77b2d1ae94409c7377573330c6d1685637
Pierre-Alexis DECALLONNE <padecallonne@gmail.com>
2021-04-19 14:34:06 +0200

    instalation via le CLI de Vue

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,9 +1,12 @@
-import Vue from 'vue';
-import App from './App.vue';
-import router from './router';
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router'
+import store from './store'
+
+Vue.config.productionTip = false
 
 new Vue({
-    el: '#app',
-    router: router,
-    template: '<router-view></router-view>'
-}).$mount('#app');
+  router,
+  store,
+  render: h => h(App)
+}).$mount('#app')
